13a5117b6a1dd21a793e55a45e1da1a3dd6179e8
ammarkhan310 <ammarkhan310@gmail.com>
2018-12-11 23:50:09 -0500

    Update functionsOnCharsAndStrings.lisp

--- standard-library/functionsOnCharsAndStrings.lisp
+++ standard-library/functionsOnCharsAndStrings.lisp
@@ -1,8 +1,8 @@
-(string-trim "khan" "khanAmmarkhan") 	; returns "Ammar"
-(string-upcase "abcde")			; returns "ABCDE"
-(string-capitalize "hello") 		; returns "Hello"
-(string= "abc" "abc") 			; returns true
-(string= "abc" "abc") 			; returns true
-(stringp "aaaaaa")			; returns true
-(stringp 123)				; returns false
-(char-code #\a)				; returns 97
+(string-trim "khan" "khanAmmarkhan")  ; returns "Ammar"
+(string-upcase "abcde")               ; returns "ABCDE"
+(string-capitalize "hello")           ; returns "Hello"
+(string= "abc" "abc")                 ; returns true
+(string= "abc" "abc")                 ; returns true
+(stringp "aaaaaa")                    ; returns true
+(stringp 123)                         ; returns false
+(char-code #\a)                       ; returns 97
